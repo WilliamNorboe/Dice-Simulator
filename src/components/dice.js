d0ecc67beff880086551389e1c071000e07533a1
William Norboe <william.norboe@gmail.com>
2023-12-14 09:01:23 -0800

    added alt text for dice

--- src/components/dice.js
+++ src/components/dice.js
@@ -31,7 +31,7 @@ function Dice (props){
     let iconPath = "./dice-" + props.top + "-solid.svg";
     return(
         <div>
-            <img src = {getFace(props.top)} className="die" />
+            <img src = {getFace(props.top)} className="die" alt = {props.top}/>
             {/* <svg xmlns={iconPath} height="16" width="14" viewBox="0 0 448 512"><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zM224 224a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>
             <FontAwesomeIcon icon={['fas', `fa-dice-${props.top}`]} className={`Die  
                 ${props.rolling && 'Die-shaking'}`} />  */}
